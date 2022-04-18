c26898b129523cdc305e87a8f94c96d2a05bf964
zishan344 <zishanahmed344@gmail.com>
2022-04-18 15:37:12 +0600

    redmi file update

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -18,3 +18,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth;
+// apiKey: process.env.REACT_APP_apiKey,
+// authDomain: process.env.REACT_APP_authDomain,
+// projectId: process.env.REACT_APP_projectId,
+// storageBucket: process.env.REACT_APP_storageBucket,
+// messagingSenderId: process.env.REACT_APP_messagingSenderId,
+// appId: process.env.REACT_APP_appId,
